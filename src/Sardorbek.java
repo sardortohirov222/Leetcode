f455bc1bda455bc44a91090f587a784bc11c9f5b
sardorbekcode <sardortohirov222@gmail.com>
2022-06-19 00:48:23 -0700

    None

--- src/Sardorbek.java
+++ src/Sardorbek.java
@@ -7,8 +7,7 @@ import java.util.stream.Stream;
 public class Sardorbek {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-
+        System.out.println("Hello World !");
     }
 }
 
