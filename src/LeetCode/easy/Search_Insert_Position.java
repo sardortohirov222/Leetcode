ae02024bd15951200160ba2bf9ec9ae45d9cd423
sardorbekcode <sardortohirov222@gmail.com>
2022-06-26 00:52:20 -0700

    Runtime: 0 ms, faster than 100.00% of Java online submissions for Search Insert Position. Memory Usage: 44.2 MB, less than 5.39% of Java online submissions for Search Insert Position.

--- src/LeetCode/easy/Search_Insert_Position.java
+++ src/LeetCode/easy/Search_Insert_Position.java
@@ -1,8 +1,25 @@
 package LeetCode.easy;
 
 public class Search_Insert_Position {
-    public int searchInsert(int[] nums, int target) {
+    public static void main(String[] args) {
+        Search_Insert_Position search_insert_position=new Search_Insert_Position();
+        System.out.println(search_insert_position.searchInsert(new int[]{1,3,5,6},13));
 
-        return 0;
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        int index=nums.length;
+        for (int i = 0; i <nums.length ; i++) {
+            if (nums[i]==target){
+                index=i;
+                break;
+            }
+            if (nums[i]>target){
+                index=i;
+                break;
+            }
+
+        }
+        return index;
     }
 }
