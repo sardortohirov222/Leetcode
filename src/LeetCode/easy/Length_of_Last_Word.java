8495c4f1af66eae5309d6ce6e6d5412421fbd8f6
sardorbekcode <sardortohirov222@gmail.com>
2022-06-30 08:02:03 -0700

    Runtime: 0 ms, faster than 100.00% of Java online submissions for Length of Last Word. Memory Usage: 42.4 MB, less than 41.41% of Java online submissions for Length of Last Word.

---
+++ src/LeetCode/easy/Length_of_Last_Word.java
@@ -0,0 +1,36 @@
+package LeetCode.easy;
+
+public class Length_of_Last_Word {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord(" asad sardorbek niisa qalzXisan      sda sdas   "));
+    }
+
+    public static int lengthOfLastWord(String s) {
+        int a = s.length() - 1;
+        int b = 0;
+        while (a > 0) {
+            if (s.charAt(a-1) == ' ' && b <= 0) {
+                a--;
+
+            } else {
+                if (s.charAt(a-1) != ' ') {
+                    b++;
+                    a--;
+                } else {
+                    break;
+                }
+
+            }
+        }
+
+
+        return b;
+    }
+//    public static int lengthOfLastWord(String s) {
+//        String[] ab= s.split(" ");
+//
+//        return ab[ab.length-1].length();
+//
+//    }
+
+}
