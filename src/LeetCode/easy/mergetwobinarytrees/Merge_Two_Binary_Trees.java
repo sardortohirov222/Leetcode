395cd75bc7240234a40b8a25bee1da735bd13aea
sardorbekcode <sardortohirov222@gmail.com>
2022-07-01 04:09:31 -0700

    Runtime: 1 ms, faster than 77.66% of Java online submissions for Merge Two Binary Trees. Memory Usage: 50.6 MB, less than 63.26% of Java online submissions for Merge Two Binary Trees.

---
+++ src/LeetCode/easy/mergetwobinarytrees/Merge_Two_Binary_Trees.java
@@ -0,0 +1,22 @@
+package LeetCode.easy.mergetwobinarytrees;
+
+import LeetCode.easy.Middle_of_the_Linked_List.ListNode;
+
+public class Merge_Two_Binary_Trees {
+    public static void main(String[] args) {
+
+        System.out.println(mergeTrees(new TreeNode(8), new TreeNode(21)));
+
+    }
+
+    public static TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 != null && root2 != null) {
+            return new TreeNode(root1.val + root2.val, mergeTrees(root1.left, root2.left), mergeTrees(root1.right, root2.right));
+        } else if (root1 == null && root2 != null) {
+            return root2;
+        } else {
+            return root1;
+        }
+    }
+
+}
