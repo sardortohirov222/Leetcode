6513743508a9c456f0424f678fde443332274094
[sardortohirov222] <sardortohirov222@gmail.com>
2022-12-19 12:52:13 -0700

    go on

--- src/LeetCode/easy/Asa.java
+++
@@ -1,9 +0,0 @@
-package LeetCode.easy;
-
-public class Asa {
-    public static void main(String[] args) {
-        System.out.println("Luck you");
-        System.out.println("Luck you");
-        System.out.println("Luck you");
-    }
-}
