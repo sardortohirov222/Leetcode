b15c756442730eb16fba69c1564fd9e53233398e
[sardortohirov222] <sardortohirov222@gmail.com>
2022-12-29 09:53:45 -0700

    1480

---
+++ src/LeetCode/easy/solved/Is_ubsequence_392.java
@@ -0,0 +1,19 @@
+package LeetCode.easy.solved;
+
+/**
+ * Date-12/29/2022
+ * Time-9:09 AM
+ */
+public class Is_ubsequence_392 {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+        while (s.length() > i && t.length() > j) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+}
