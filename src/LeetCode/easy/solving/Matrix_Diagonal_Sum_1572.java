a9f59886d8a2f21462edbe724762c6cf1fdd566c
[sardortohirov222] <sardortohirov222@gmail.com>
2023-01-18 09:51:15 -0700

    I love My self!

---
+++ src/LeetCode/easy/solving/Matrix_Diagonal_Sum_1572.java
@@ -0,0 +1,27 @@
+package LeetCode.easy.solving;
+
+/**
+ * Date-1/18/2023
+ * Time-9:14 AM
+ */
+public class Matrix_Diagonal_Sum_1572 {
+
+    public int diagonalSum(int[][] mat) {
+        int leftSideSum = 0;
+        int rightSideSum = 0;
+        int lenR = mat.length - 1;
+        int lenRDuble = 0;
+        int lenL = 0;
+        while (lenR >= 0) {
+            leftSideSum += mat[lenL][lenL];
+            rightSideSum += mat[lenL][lenR];
+            lenL++;
+            lenR--;
+        }
+        if (mat.length % 2 == 1) {
+            int i = mat.length / 2;
+            leftSideSum -= mat[i][i];
+        }
+        return leftSideSum + rightSideSum;
+    }
+}
