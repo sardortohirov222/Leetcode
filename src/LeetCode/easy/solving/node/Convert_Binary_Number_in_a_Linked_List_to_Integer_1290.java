dac617f474eb8bde1c13bc84b41f303493152b26
[sardortohirov222] <sardortohirov222@gmail.com>
2023-01-21 08:53:00 -0700

    1290

---
+++ src/LeetCode/easy/solving/node/Convert_Binary_Number_in_a_Linked_List_to_Integer_1290.java
@@ -0,0 +1,10 @@
+public class Convert_Binary_Number_in_a_Linked_List_to_Integer_1290 {
+    public int getDecimalValue(ListNode head) {
+        String a="";
+        while(head!=null){
+            a+=head.val;
+            head=head.next;
+        }
+        return Integer.parseInt(a,2);
+    }
+}
