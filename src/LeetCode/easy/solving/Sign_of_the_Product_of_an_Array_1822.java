be8182d981eaa502a6b188d5d5b718f978023384
[sardortohirov222] <sardortohirov222@gmail.com>
2023-01-22 09:07:34 -0700

    1822

---
+++ src/LeetCode/easy/solving/Sign_of_the_Product_of_an_Array_1822.java
@@ -0,0 +1,16 @@
+package LeetCode.easy.solving;
+
+public class Sign_of_the_Product_of_an_Array_1822 {
+    public int arraySign(int[] nums) {
+        int product=1;
+        for(int num:nums){
+           if(num<0){
+            product=-1;
+           }else if(num==0){
+            return 0;
+           }
+        }
+        return product;
+    }
+    }
+    
---
+++ src/LeetCode/easy/solving/Sort_Integers_by_The_Number_of_1_Bits_1356.java
@@ -0,0 +1,23 @@
+package LeetCode.easy.solving;
+
+public class Sort_Integers_by_The_Number_of_1_Bits_1356 {
+public static void main(String[] args) {
+    System.out.println(Integer.toBinaryString(0));
+    System.out.println(Integer.toBinaryString(2));
+    System.out.println(Integer.toBinaryString(3));
+    System.out.println(Integer.toBinaryString(4));
+    System.out.println(Integer.toBinaryString(1));
+    System.out.println(Integer.toBinaryString(5));
+    System.out.println(Integer.toBinaryString(6));
+    System.out.println(Integer.toBinaryString(7));
+    System.out.println(Integer.toBinaryString(8));
+    System.out.println(Integer.toBinaryString(9));
+}
+
+    public int[] sortByBits(int[] arr) {
+       return null; 
+    }
+
+
+    
+}
