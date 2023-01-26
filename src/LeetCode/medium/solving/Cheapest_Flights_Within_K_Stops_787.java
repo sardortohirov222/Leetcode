a8d746c1195c4c15401541372e65c7e9ae2f6dff
[sardortohirov222] <sardortohirov222@gmail.com>
2023-01-26 09:37:50 -0700

    i will commit this problem soon.... !

---
+++ src/LeetCode/medium/solving/Cheapest_Flights_Within_K_Stops_787.java
@@ -0,0 +1,11 @@
+package LeetCode.medium.solving;
+
+/**
+ * Date-1/26/2023
+ * Time-8:53 AM
+ */
+public class Cheapest_Flights_Within_K_Stops_787 {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        return 400;
+    }
+}
