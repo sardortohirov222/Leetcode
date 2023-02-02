4150b883a8d9cb7af38e7e719941d07b2ce7be8c
[sardortohirov222] <sardortohirov222@gmail.com>
2023-02-02 11:12:12 -0700

    daily challange is solved

---
+++ src/LeetCode/easy/solving/Verifying_an_Alien_Dictionary_953.java
@@ -0,0 +1,14 @@
+package LeetCode.easy.solving;
+
+import java.util.HashMap;
+
+/**
+ * Date-2/2/2023
+ * Time-8:33 AM
+ */
+public class Verifying_an_Alien_Dictionary_953 {
+    public boolean isAlienSorted(String[] words, String order) {
+
+    }
+    
+}
