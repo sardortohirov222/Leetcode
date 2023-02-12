b66544d82c76df462b99cd58625e2e14505fa485
[sardortohirov222] <sardortohirov222@gmail.com>
2023-02-12 10:01:06 -0700

    daily challange is solved

---
+++ src/LeetCode/medium/Shortest_Path_with_Alternating_Colors_1129.java
@@ -0,0 +1,12 @@
+package LeetCode.medium;
+
+/**
+ * Date-2/11/2023
+ * Time-8:31 AM
+ */
+public class Shortest_Path_with_Alternating_Colors_1129 {
+    public int[] shortestAlternatingPaths(int n, int[][] redEdges, int[][] blueEdges) {
+
+        return null;
+    }
+}
