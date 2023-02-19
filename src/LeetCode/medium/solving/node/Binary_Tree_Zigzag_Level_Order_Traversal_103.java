0fe1061f2b452377759edf2e41f06e7d405110b8
[sardortohirov222] <sardortohirov222@gmail.com>
2023-02-19 10:02:39 -0700

    daily challange is solved

---
+++ src/LeetCode/medium/solving/node/Binary_Tree_Zigzag_Level_Order_Traversal_103.java
@@ -0,0 +1,16 @@
+package LeetCode.medium.solving.node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Date-2/19/2023
+ * Time-9:20 AM
+ */
+public class Binary_Tree_Zigzag_Level_Order_Traversal_103 {
+    List<List<Integer>> listList=new ArrayList<>();
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        return listList;
+    }
+//    public
+}
---
+++ src/LeetCode/medium/solving/node/TreeNode.java
@@ -0,0 +1,24 @@
+package LeetCode.medium.solving.node;
+
+/**
+ * Date-2/19/2023
+ * Time-9:20 AM
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
